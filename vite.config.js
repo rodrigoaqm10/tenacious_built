d777b4400bde87ecc09780570f945b88963072d6
rodrigoaqm10 <rodrigoaqm10@gmail.com>
2026-05-30 18:55:18 -0400

    config: switch to Vercel deployment - base path to root

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tenacious_built/', // Debe ser el nombre exacto de tu repositorio
+  base: '/',
 })
